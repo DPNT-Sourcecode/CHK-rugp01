0aff289478668ba558b5bd11caeeaf65dfd262c3
sbx_user1051 <sbx_user1051@169.254.13.17>
2025-11-08 16:51:30 +0000

    Sat Nov 08 16:51:30 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,12 +1,23 @@
-
 class CheckoutSolution:
 
     # skus = unicode string
-    def checkout(self, skus:str) -> int:
+    def checkout(self, skus: str) -> int:
         # Rules
         # A = 50, 3A = 130
         # B = 30, 2B = 45
         # C = 20, D = 15
 
-        
+        if not all(ch.isalpha() for ch in skus):
+            return -1
+
+        prices = {
+            'A': 50,
+            'B': 30,
+            'C': 20,
+            'D': 15
+        }
+        offers = {
+            
+        }
+
 
