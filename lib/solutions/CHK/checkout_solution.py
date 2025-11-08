a22c17a71fb5ce65464b8b276154bd52a9844355
sbx_user1051 <sbx_user1051@169.254.13.17>
2025-11-08 16:48:30 +0000

    Sat Nov 08 16:48:30 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,11 @@
 class CheckoutSolution:
 
     # skus = unicode string
-    def checkout(self, skus):
-        raise NotImplementedError()
+    def checkout(self, skus:str) -> int:
+        # Rules
+        # A = 50, 3A = 130
+        # B = 30, 2B = 45
+        # C = 20, D = 15
+
+        
+
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,8 +1,6 @@
-import unittest
+import pytest
+from lib.solutions.CHK import checkout_solution
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+def test_checkout():
+    assert checkout_solution("A") == 50
 
-if __name__ == '__main__':
-    unittest.main()
